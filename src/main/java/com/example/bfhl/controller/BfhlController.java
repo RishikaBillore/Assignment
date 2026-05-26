4bfc4e5698f42ef4ee52af260e1ad9953d7fc3c2
Rishika Billore <rishikabillore2006@gmail.com>
2026-05-26 15:45:49 +0530

    added health endpoint

--- src/main/java/com/example/bfhl/controller/BfhlController.java
+++ src/main/java/com/example/bfhl/controller/BfhlController.java
@@ -6,14 +6,23 @@ import com.example.bfhl.service.BfhlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/bfhl")
+@RequestMapping
 public class BfhlController {
 
     @Autowired
     private BfhlService bfhlService;
 
-    @PostMapping
+    @GetMapping("/health")
+    public Map<String, String> health() {
+        return Map.of(
+                "status", "UP"
+        );
+    }
+
+    @PostMapping("/bfhl")
     public ResponseDto process(@RequestBody RequestDto request) {
         return bfhlService.processData(request);
     }
